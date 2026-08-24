fdd7f50eba3cfe9dc4a0876137262fa4163bb49d
Ivan Sorokin <existfragger@gmail.com>
2026-08-24 13:18:52 +0300

    update sha256: rebuild with sanitized Info.plist

--- Casks/macmqtt.rb
+++ Casks/macmqtt.rb
@@ -1,6 +1,6 @@
 cask "macmqtt" do
   version "0.1.0"
-  sha256 "559ea5a82d974aa0fd307debb99e3105afd0e3d7913d128fc5cdbcdc09e590d2"
+  sha256 "5ad4cd88f0bf8bf8c0da5b75c43798919dff7bb6704f5b123938ed02cd929496"
 
   url "https://github.com/eXist-FraGGer/macmqtt/releases/download/v#{version}/macmqtt-#{version}.zip"
   name "macmqtt"
