834d486ae2a3a403dfc9d261b9b9574a9bc3562d
Ivan Sorokin <existfragger@gmail.com>
2026-09-15 19:47:04 +0300

    bump to v0.2.11

--- Casks/macmqtt.rb
+++ Casks/macmqtt.rb
@@ -1,6 +1,6 @@
 cask "macmqtt" do
-  version "0.2.10"
-  sha256 "3b94132be7128d9f95de4a95416708d372ed3cc8fda7c52b2cbaf977e1789acd"
+  version "0.2.11"
+  sha256 "7331a1f833c78954181e017741b80e622dbd34793395cda3754c57e917bb3d2e"
 
   url "https://github.com/eXist-FraGGer/macmqtt/releases/download/v#{version}/macmqtt-#{version}.zip"
   name "macmqtt"
